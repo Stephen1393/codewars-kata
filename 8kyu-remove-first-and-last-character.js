f7c32488501c2750c3bd009bac48b6abbd6e34a7
Stephen1393 <stephen.bending20@gmail.com>
2025-11-21 14:56:00 +0000

    Solve 8kyu Remove First and Last Character kata with tests

---
+++ 8kyu-remove-first-and-last-character.js
@@ -0,0 +1,12 @@
+function removeChar(str){
+  let word = ""
+ for ( let i = 1; i < str.length - 1; i++) {
+    word += str[i]
+   
+   
+ }
+  return word
+
+};
+
+module.exports = {removeChar};
---
+++ __tests__/remove-first-and-last-character.test.js
@@ -0,0 +1,23 @@
+const { removeChar } = require('../8kyu-remove-first-and-last-character');
+
+
+test('removes first and last character from "eloquent"', () => {
+  expect(removeChar("eloquent")).toBe("loquen");
+});
+
+test('removes first and last character from "country"', () => {
+    expect(removeChar("country")).toBe("ountr");
+});
+
+test('removes first and last character from "ooopsss"', () => {
+    expect(removeChar("ooopsss")).toBe("oopss");
+});
+
+test('removes first and last character from "ab"', () => {
+    expect(removeChar("ab")).toBe("");
+});
+
+test('removes first and last character from "abc"', () => {
+    expect(removeChar("abc")).toBe("b");
+});
+
